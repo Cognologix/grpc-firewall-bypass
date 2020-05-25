4c6433236db2afa8657b2dc047eba9e3afc75cbf
Chetan Chaudhari <chetanrchaudhari@gmail.com>
2020-05-25 14:58:59 +0530

    updated for rest server

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/golang/protobuf v1.4.2
 	github.com/hashicorp/yamux v0.0.0-20190923154419-df201c70410d
+        github.com/gorilla/mux v1.7.2
 	golang.org/x/net v0.0.0-20200519113804-d87ec0cfa476
 	golang.org/x/sys v0.0.0-20200519105757-fe76b779f299 // indirect
 	golang.org/x/text v0.3.2 // indirect
--- server/main.go
+++ server/main.go
@@ -1,19 +1,33 @@
 package main
 
 import (
-	"context"
-	"github.com/hashicorp/yamux"
-	"google.golang.org/grpc"
 	"cognologix.com/grpc-firewall-bypass/api"
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/hashicorp/yamux"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
 	"log"
 	"net"
+	"net/http"
 	"time"
 )
 
-// TCP server and GRPC client
+var m = make(map[string]api.PingClient)
 
-func main() {
+func handleRequests() {
+	log.Println("Starting REST API ...")
+	// creates a new instance of a mux router
+	myRouter := mux.NewRouter().StrictSlash(true)
+	// replace http.HandleFunc with myRouter.HandleFunc
+	myRouter.HandleFunc("/command/{serverAddress}/{commandString}", runCommand)
+	// finally, instead of passing in nil, we want
+	// to pass in our newly created router as the second
+	// argument
+	log.Fatal(http.ListenAndServe(":10000", myRouter))
+}
 
+func initGrpcServer() {
 	log.Println("launching tcp server...")
 
 	// start tcp listener on all interfaces
@@ -40,37 +54,63 @@ func main() {
 
 		log.Println("starting a gRPC server over incoming TCP connection")
 
+		var address net.Addr
+		address = incomingConn.RemoteAddr()
+		var stringAddress = address.String()
+		log.Println("Remote Address :: " + stringAddress)
 		var conn *grpc.ClientConn
 		// gRPC dial over incoming net.Conn
 		conn, err = grpc.Dial(":7777", grpc.WithInsecure(),
 			grpc.WithDialer(func(target string, timeout time.Duration) (net.Conn, error) {
-				var address net.Addr
-				address = incomingConn.RemoteAddr()
-				log.Println("Remote Address :: " + address.String())
 				return incomingConn.Open()
 			}),
 		)
-
-
 
 		if err != nil {
 			log.Fatalf("did not connect: %s", err)
 		}
 
+		//defer conn.Close()
+		c := api.NewPingClient(conn)
+		m[stringAddress] = c
 		// handle connection in goroutine so we can accept new TCP connections
-		go handleConn(conn)
+		//go handleConn(conn, stringAddress, "Get")
 	}
 }
 
-func handleConn(conn *grpc.ClientConn) {
-	defer conn.Close()
-	c := api.NewPingClient(conn)
-	for i := 0; i < 10; i++ {
-		response, err := c.RunCommand(context.Background(), &api.CommandMessage{Command: "setConfigValue"})
-		if err != nil {
-			log.Fatalf("error when calling RunCommand: %s", err)
-		}
-		log.Printf("response from server: %s", response.CommandResult)
+func runCommand(w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)
+	command := vars["commandString"]
+	serverAddress := vars["serverAddress"]
+	var output = handleConn(m[serverAddress], serverAddress, command)
+	fmt.Fprintf(w, output)
+}
+
+func handleConn(c api.PingClient, serverAddress string, command string ) string {
+	//defer conn.Close()
+	//c := api.NewPingClient(conn)
+
+	response, err := c.RunCommand(context.Background(), &api.CommandMessage{Command: command})
+	if err != nil {
+		log.Fatalf("error when calling RunCommand: %s", err)
 	}
+	log.Printf("response from server address: %s , Command output is %s", serverAddress, response.CommandResult)
+	return "From Device: \n\nServerAddress: " + serverAddress  +
+		" \n\nCommand Sent to Device = " + command +
+		" \n\nCommand Output = " + response.CommandResult
+
+
+
 
 }
+
+// TCP server and GRPC client
+func main() {
+
+	fmt.Println("Rest API v2.0 - Mux Routers")
+	go initGrpcServer()
+
+	handleRequests()
+
+}
+
